5c47ce6315dd4c8c9e4073c043d7dc405c78fd4e
Christian Asmussen <github@cwa.to>
2014-10-25 23:51:53 +0200

    added gitignore

---
+++ krico-test/src/main/java/to/cwa/krico_test/GuestbookServlet.java
@@ -0,0 +1,39 @@
+package to.cwa.krico_test;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by krico on 10/25/14.
+ */
+public class GuestbookServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        if (req.getParameter("testing") == null) {
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Hello, this is a testing servlet. \n\n");
+            Properties p = System.getProperties();
+            p.list(resp.getWriter());
+
+        } else {
+            UserService userService = UserServiceFactory.getUserService();
+            User currentUser = userService.getCurrentUser();
+
+            if (currentUser != null) {
+                resp.setContentType("text/plain");
+                resp.getWriter().println("Hello, " + currentUser.getNickname());
+            } else {
+                resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+            }
+        }
+    }
+}
