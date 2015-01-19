9c672aa646a7ee89e68020b490aa4a292a2c0048
krico <github@cwa.to>
2015-01-20 00:00:34 +0100

    Layout for new Sign Up / Create Account process finished. Fixes #103

---
+++ schedule/schedule-appengine/src/test/client/authenticate/sign-in.spec.js
@@ -0,0 +1,23 @@
+describe('SignInController', function () {
+    var vm;
+    beforeEach(module('jasify'));
+
+    beforeEach(inject(function ($controller) {
+        vm = $controller('SignInController');
+    }));
+
+    it('is not on e-mail mode when instantiated', function () {
+        expect(vm.isEmail()).toBe(false);
+    });
+
+    it('switches to email', function () {
+        vm.withEmail();
+        expect(vm.isEmail()).toBe(true);
+    });
+
+    it('initializes user', function () {
+        expect(typeof vm.user).toEqual('object');
+    });
+
+
+});
