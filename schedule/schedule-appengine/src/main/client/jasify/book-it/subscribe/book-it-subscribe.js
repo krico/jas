2ed68b52462493b7361db071294cf14efcd80717
krico <github@cwa.to>
2015-01-26 22:31:03 +0100

    "Book it!" works now, fixes #135

--- schedule/schedule-appengine/src/main/client/jasify/book-it/subscribe/book-it-subscribe.js
+++ schedule/schedule-appengine/src/main/client/jasify/book-it/subscribe/book-it-subscribe.js
@@ -2,13 +2,54 @@
 
     angular.module('jasify.bookIt').controller('BookItSubscribeController', BookItSubscribeController);
 
-    function BookItSubscribeController(activity) {
+    function BookItSubscribeController(Session, Activity, activity) {
         var vm = this;
         vm.activity = activity;
         vm.bookIt = bookIt;
+        vm.checkSubscribed = checkSubscribed;
+        vm.inProgress = false;
+        vm.subscription = null;
+        vm.showBookIt = showBookIt;
+
+        vm.checkSubscribed();
+
+        function showBookIt() {
+            if (vm.subscription === null) {
+                return true;
+            }
+            return !vm.subscription.id;
+
+        }
+
+        function checkSubscribed() {
+            if (vm.activity.id) {
+                vm.inProgress = true;
+                Activity.isSubscribed(Session.userId, vm.activity).then(subscribed, notSubscribed);
+            }
+
+            function subscribed(subscription) {
+                vm.inProgress = false;
+                vm.subscription = subscription;
+            }
+
+            function notSubscribed() {
+                vm.inProgress = false;
+            }
+        }
 
         function bookIt() {
-            alert('TODO: book it!');
+            vm.inProgress = true;
+            Activity.subscribe(Session.userId, vm.activity).then(subscribed, failed);
+
+            function subscribed(subscription) {
+                vm.inProgress = false;
+                vm.subscription = subscription;
+            }
+
+            function failed() {
+                vm.inProgress = false;
+                //TODO: handle error
+            }
         }
     }
 })(angular);
