d61b9c20978cd33e30388096f71cc49977ec3163
Waldemar <wszarmach@wp.pl>
2015-03-21 20:25:48 +0100

    Fixes #181 Fixes #179 Fixes #176 Fixes #171 Fixes #142

---
+++ schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/model/OperationException.java
@@ -0,0 +1,11 @@
+package com.jasify.schedule.appengine.model;
+
+/**
+ * @author wszarmach
+ * @since 17/03/15.
+ */
+public class OperationException extends ModelException {
+    public OperationException(String message) {
+        super(message);
+    }
+}
