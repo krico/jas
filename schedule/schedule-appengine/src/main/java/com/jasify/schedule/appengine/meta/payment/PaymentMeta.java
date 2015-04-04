a000bc433a8f49287050da27239b1fae91602a63
krico <github@cwa.to>
2015-04-04 16:26:53 +0200

    Local repository with our own version of slim3-gen that doesn't put the date on the generated code to close #222

--- schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/meta/payment/PaymentMeta.java
+++ schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/meta/payment/PaymentMeta.java
@@ -1,6 +1,6 @@
 package com.jasify.schedule.appengine.meta.payment;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2015-03-22 13:08:07")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" })
 /** */
 public final class PaymentMeta extends org.slim3.datastore.ModelMeta<com.jasify.schedule.appengine.model.payment.Payment> {
 
