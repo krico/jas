b4427ee841b065f1ef8f99df25feec31056848d0
krico <github@cwa.to>
2015-04-20 00:38:49 +0200

    Changed to go into schedule/schedule-appengine dir

--- scripts/build-runner.sh
+++ scripts/build-runner.sh
@@ -10,6 +10,7 @@ if [ "${JAS_BUILD_MODE}" = "e2e" ];
 then
   if [ "${TRAVIS_PULL_REQUEST}" = "false" ];
   then
+    cd schedule/schedule-appengine;
     exec ./node_modules/.bin/protractor src/test/e2e/protractor.cfg.js --suite ci
   else
     echo "Cannot run e2e tests on pull requests...";
--- scripts/build-setup.sh
+++ scripts/build-setup.sh
@@ -3,6 +3,6 @@ set -ev
 
 if [ "${JAS_BUILD_MODE}" = "e2e" -a "${TRAVIS_PULL_REQUEST}" = "false" ];
 then
-  (mvn appengine:devserver &) || /bin/true
+  (mvn -f schedule/schedule-appengine/pom.xml appengine:devserver &) || /bin/true
   sleep 30 # let appengine fire up
 fi
