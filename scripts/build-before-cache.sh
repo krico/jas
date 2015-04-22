4688312e4cc27486065c5a0d9fd8d6e05acfdc10
krico <github@cwa.to>
2015-04-23 01:45:49 +0200

    get the run log after we execute

--- scripts/build-before-cache.sh
+++ scripts/build-before-cache.sh
@@ -2,5 +2,9 @@
 rm -f $HOME/.m2/repository/org/codehaus/mojo/versions-maven-plugin/maven-metadata-central.xml
 find $HOME/.m2/repository -name _remote.repositories -exec rm -f {} \;
 mkdir -p /tmp/cache-trick/
-mv $HOME/.m2/repository/com/jasify /tmp/cache-trick/
+rm -rf $HOME/.m2/repository/com/jasify $HOME/.npm/abbrev
+echo "Checking cache sizes"
+
+du -hs $HOME/.m2 $HOME/.bower $HOME/.npm schedule/schedule-appengine/node schedule/schedule-appengine/node_modules
+
 exit 0
