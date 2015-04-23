eea2af02680e61d0f052df10f05bdfff1c184bbd
krico <github@cwa.to>
2015-04-24 00:42:00 +0200

    run bower install from mvn before the gulp build

--- scripts/build-before-cache.sh
+++ scripts/build-before-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-find $HOME/.m2/ -name _remote.repositories -exec rm -f {} \;
-rm -rf $HOME/.m2/com/jasify
+find $HOME/.m2/repository/ -name _remote.repositories -exec rm -f {} \;
+rm -rf $HOME/.m2/repository/com/jasify
 echo "Checking cache sizes"
 du -hs $JAS_CACHE $JAS_CACHE/* $JAS_CACHE/*/*
 exit 0
--- scripts/build-install.sh
+++ scripts/build-install.sh
@@ -15,18 +15,18 @@ ln -s "$JAS_CACHE/schedule/schedule-appengine/node_modules" schedule/schedule-ap
 
 echo "Setting up links for $HOME/.m2"
 rm -rf $HOME/.m2
-mkdir -p "$JAS_CACHE/.m2"
-ln -s "$JAS_CACHE/.m2" $HOME/.m2
+mkdir -p "$JAS_CACHE/HOME.m2"
+ln -s "$JAS_CACHE/HOME.m2" $HOME/.m2
 
 echo "Setting up links for $HOME/.npm"
 rm -rf $HOME/.npm
-mkdir -p "$JAS_CACHE/.npm"
-ln -s "$JAS_CACHE/.npm" $HOME/.npm
+mkdir -p "$JAS_CACHE/HOME.npm"
+ln -s "$JAS_CACHE/HOME.npm" $HOME/.npm
 
 echo "Setting up links for $HOME/.bower"
 rm -rf $HOME/.bower
-mkdir -p "$JAS_CACHE/.bower"
-ln -s "$JAS_CACHE/.bower" $HOME/.bower
+mkdir -p "$JAS_CACHE/HOME.bower"
+ln -s "$JAS_CACHE/HOME.bower" $HOME/.bower
 
 
 
