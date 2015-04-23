d79df803a9da8f84b8089022f082db5f2523d4d7
krico <github@cwa.to>
2015-04-23 22:31:31 +0200

    Trying out a single cache directory to speed up build

--- scripts/build-before-cache.sh
+++ scripts/build-before-cache.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#rm -f $HOME/.m2/repository/org/codehaus/mojo/versions-maven-plugin/maven-metadata-central.xml
-find $HOME/.m2/repository -name _remote.repositories -exec rm -f {} \;
-rm -rf $HOME/.m2/repository/com/jasify
+find $MAVEN_REPOSITORY -name _remote.repositories -exec rm -f {} \;
+rm -rf $MAVEN_REPOSITORY/com/jasify
 echo "Checking cache sizes"
-du -hs $HOME/.m2 $HOME/.bower $HOME/.npm schedule/schedule-appengine/node schedule/schedule-appengine/node_modules
-
+du -hs $JAS_CACHE/*/*
 exit 0
--- scripts/build-install.sh
+++ scripts/build-install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-set -ev
+set -e
+
 if [ "${JAS_BUILD_MODE}" = "ci" ];
 then
+  mkdir -p schedule/schedule-appengine/node
+  mkdir -p schedule/schedule-appengine/node_modules
+  rm -rf schedule/schedule-appengine/node schedule/schedule-appengine/node_modules
+  mkdir -p $JAS_CACHE/schedule/schedule-appengine/node
+  ln -s $JAS_CACHE/schedule/schedule-appengine/node schedule/schedule-appengine/node
+  mkdir -p $JAS_CACHE/schedule/schedule-appengine/node_modules
+  ln -s $JAS_CACHE/schedule/schedule-appengine/node_modules schedule/schedule-appengine/node_modules
   exec mvn install -DskipTests=true
 fi
