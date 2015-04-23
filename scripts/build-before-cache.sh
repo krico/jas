41f1ede6772c509146aa7a9d997edde8e0e7a86d
krico <github@cwa.to>
2015-04-24 00:55:50 +0200

    And now the final test of the build speedup

--- scripts/build-before-cache.sh
+++ scripts/build-before-cache.sh
@@ -2,5 +2,7 @@
 find $HOME/.m2/repository/ -name _remote.repositories -exec rm -f {} \;
 rm -rf $HOME/.m2/repository/com/jasify
 echo "Checking cache sizes"
-du -hs $JAS_CACHE $JAS_CACHE/* $JAS_CACHE/*/*
+du -hs $JAS_CACHE
+du -hs $JAS_CACHE/*
+du -hs $JAS_CACHE/*/*
 exit 0
