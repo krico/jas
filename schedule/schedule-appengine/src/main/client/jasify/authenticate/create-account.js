93f38ee7c85f6a9e862ed6d17eefe874a0c591b3
Paweł <waszczynski@gmail.com>
2015-04-27 23:28:59 +0200

    - simplify create-account controller & touch page

--- schedule/schedule-appengine/src/main/client/jasify/authenticate/create-account.js
+++ schedule/schedule-appengine/src/main/client/jasify/authenticate/create-account.js
@@ -3,117 +3,55 @@
     angular.module('jasify.authenticate').controller('CreateAccountController', CreateAccountController);
 
     function CreateAccountController($rootScope, $window, Auth, User) {
+
         var vm = this;
         vm.user = {};
         vm.authenticateForm = {};
         vm.email = false;
-        vm.showErrors = false;
-        vm.withEmail = withEmail;
-        vm.withOAuth = withOAuth;
-        vm.isEmail = isEmail;
         vm.passwordStrengthCallback = passwordStrengthCallback;
-        vm.popoverText = '';
-        vm.hasSuccess = hasSuccess;
-        vm.hasError = hasError;
         vm.create = create;
-        vm.getTooltip = getTooltip;
-        vm.inProgress = false;
         vm.oauth = oauth;
-        vm.alert = function (type, message) {
-            //TODO:
-            console.log("Alert[" + type + "]:" + message);
-        };
 
         function oauth(provider, cb) {
-            Auth.providerAuthorize(provider).then(redirect, fail);
-            function redirect(resp) {
+            Auth.providerAuthorize(provider).then(function (resp) {
                 $window.location.href = resp.result.authorizeUrl;
-            }
-
-            function fail(reason) {
+            }, function () {
                 $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
-            }
+            });
         }
 
-
         function create(cb) {
-            if (vm.authenticateForm.$invalid) {
-                vm.showErrors = true;
-                return;
-            }
-            vm.inProgress = true;
-
-            vm.user.name = vm.user.email; //username == email
+            vm.user.name = vm.user.email;
             User.add(vm.user, vm.user.password).then(saveSuccess, saveError);
 
             function saveSuccess(ret) {
-                vm.inProgress = false;
                 if (angular.isFunction(cb)) {
                     cb();
                 }
             }
 
-            //User.save error
             function saveError(httpResponse) {
-                vm.inProgress = false;
-
-                vm.alert('danger', ":-( registration failed, since this was really unexpected, please change some fields and try again.");
-
-            }
-
-        }
-
-        function getTooltip() {
-            if (vm.authenticateForm.$invalid) {
-                return 'Please fix the errors.';
-            } else {
-                return '';
+                // TODO: create http interceptor to handler errors
             }
         }
-
-        function hasSuccess(fieldName) {
-            if (vm.authenticateForm[fieldName]) {
-                var f = vm.authenticateForm[fieldName];
-                return f && (f.$dirty || vm.showErrors) && f.$valid;
-            } else {
-                return false;
-            }
-        }
-
-        function hasError(fieldName) {
-            if (vm.authenticateForm[fieldName]) {
-                var f = vm.authenticateForm[fieldName];
-                return f && (f.$dirty || vm.showErrors) && f.$invalid;
-            } else {
-                return false;
-            }
-        }
-
 
         function passwordStrengthCallback(s) {
             if (s <= 0) {
-                vm.popoverText = 'Choose a password.';
+                vm.passwordStrength = null;
+                vm.passwordStrengthText = null;
             } else if (s <= 15) {
-                vm.popoverText = 'Weak!';
+                vm.passwordStrength = 'weak';
+                vm.passwordStrengthText = 'Weak!';
             } else if (s <= 40) {
-                vm.popoverText = 'Average...';
+                vm.passwordStrength = 'average';
+                vm.passwordStrengthText = 'Average...';
             } else if (s <= 80) {
-                vm.popoverText = 'Good!';
+                vm.passwordStrength = 'good';
+                vm.passwordStrengthText = 'Good!';
             } else {
-                vm.popoverText = 'Excellent!!!';
+                vm.passwordStrength = 'good';
+                vm.passwordStrengthText = 'Excellent!!!';
             }
-        }
-
-        function withOAuth() {
-            vm.email = false;
-        }
-
-        function withEmail() {
-            vm.email = true;
-        }
-
-        function isEmail() {
-            return vm.email;
         }
     }
 })(angular);
--- schedule/schedule-appengine/src/main/client/jasify/directives.js
+++ schedule/schedule-appengine/src/main/client/jasify/directives.js
@@ -1,3 +1,131 @@
-/**
- * Created by Pawcio on 2015-04-26.
- */
+(function (angular, $) {
+    var jasifyDirectivesFormModule = angular.module('jasify.directives.form', []);
+
+    jasifyDirectivesFormModule.run(function() {
+        $('body').on('focus', '.form-control', function(){
+            $(this).closest('.fg-line').addClass('fg-toggled');
+        });
+
+        $('body').on('blur', '.form-control', function(){
+            var p = $(this).closest('.form-group');
+            var i = p.find('.form-control').val();
+
+            if (p.hasClass('fg-float')) {
+                if (i.length === 0) {
+                    $(this).closest('.fg-line').removeClass('fg-toggled');
+                }
+            } else {
+                $(this).closest('.fg-line').removeClass('fg-toggled');
+            }
+        });
+    });
+
+    jasifyDirectivesFormModule.directive('fg-float', function() {
+        return {
+            restrict: 'C',
+            link: function(scope, element, attrs) {
+                console.log(element);
+                $('.form-control', element).each(function(){
+                    var i = $(this).val();
+
+                    if (i.length !== 0) {
+                        $(this).closest('.fg-line').addClass('fg-toggled');
+                    }
+                });
+            }
+        };
+    });
+
+    jasifyDirectivesFormModule.directive('jasErrorHelpBlock', function() {
+        return {
+            replace: true,
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                field: '=',
+                form: '='
+            },
+            template: '<small><span class="help-block has-error"' +
+            'ng-if="(form.$submitted || field.$touched) && field.$invalid"' +
+            'ng-messages=field.$error><ng-transclude></ng-transclude></span></small>'
+        };
+    });
+
+    jasifyDirectivesFormModule.directive('jasHasFeedback', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+
+                var $element = $(element);
+
+                var formName = $element.closest('form').attr('name');
+                var formFieldName = $element.find('input, select, textarea').attr('name');
+
+                scope.$watch(formName + '.' + formFieldName + '.$touched', function() {
+                    updateClasses();
+                });
+
+                scope.$watch(formName + '.' + formFieldName + '.$valid', function() {
+                    updateClasses();
+                });
+
+                scope.$watch(formName + '.$submitted', function() {
+                    updateClasses();
+                });
+
+                function updateClasses() {
+                    var form = scope.$eval(formName);
+                    console.log(form[formFieldName].$touched, form[formFieldName].$valid)
+                    if (form[formFieldName].$touched && form[formFieldName].$valid) {
+                        $element.addClass('has-success');
+                    } else {
+                        $element.removeClass('has-success');
+                    }
+
+                    if ((form.$submitted || form[formFieldName].$touched) && form[formFieldName].$invalid) {
+                        $element.addClass('has-error');
+                    } else {
+                        $element.removeClass('has-error');
+                    }
+                }
+            }
+        };
+    });
+
+    jasifyDirectivesFormModule.directive('jasFeedbackIconValid', function() {
+        return {
+            multiElement: true,
+            restrict: 'E',
+            replace: true,
+            scope: {
+                field: '='
+            },
+            template: '<i ng-show="field.$valid && field.$touched" class="md-icon md-icon-check form-control-feedback"></i>'
+        };
+    });
+
+    jasifyDirectivesFormModule.directive('jasFeedbackIconInvalid', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                field: '=',
+                form: '='
+            },
+            template: '<i ng-show="(form.$submitted || field.$touched) && field.$invalid" class="md-icon md-icon-close form-control-feedback"></i>'
+        };
+    });
+
+    jasifyDirectivesFormModule.directive('jasFeedbackIconPending', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                field: '='
+            },
+            template: '<span class="form-control-feedback" ng-show="field.$pending"><span us-spinner="{lines:9, length:5, width:2, radius:3, trail: 40}"></span></span>'
+        };
+    });
+
+}(angular, jQuery));
+
