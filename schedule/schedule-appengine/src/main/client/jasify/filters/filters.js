54f231327001241456c3590670bc661277551826
Paweł <waszczynski@gmail.com>
2015-05-11 22:30:25 +0200

    Rename filters module to follow convention

--- schedule/schedule-appengine/src/main/client/jasify/filters/filters.js
+++ schedule/schedule-appengine/src/main/client/jasify/filters/filters.js
@@ -1,6 +1,6 @@
 (function (angular) {
 
-    angular.module('jasifyFilters', [])
+    angular.module('jasify.filters', [])
         .filter('tableShortDateTime', tableShortDateTime);
 
     function tableShortDateTime($filter) {
@@ -8,9 +8,9 @@
             var from = value[0],
                 to = value[1],
                 fromDate = $filter('date')(from, 'EEE, d MMM'),
-                toDate = $filter('date')(from, 'EEE, d MMM'),
+                toDate = $filter('date')(to, 'EEE, d MMM'),
                 fromTime = $filter('date')(from, 'h:mm a'),
-                toTime = $filter('date')(from, 'h:mm a');
+                toTime = $filter('date')(to, 'h:mm a');
 
             if (fromDate === toDate) {
                 return fromDate + ' ' + fromTime + ' - ' + toTime;
