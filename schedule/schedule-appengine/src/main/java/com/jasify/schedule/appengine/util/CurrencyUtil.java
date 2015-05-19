f37195224d177a7788289215684e3296344d599f
krico <github@cwa.to>
2015-05-19 23:38:01 +0200

    Specify locale when formatting amount, fixes #392

--- schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/util/CurrencyUtil.java
+++ schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/util/CurrencyUtil.java
@@ -3,6 +3,8 @@ package com.jasify.schedule.appengine.util;
 import com.google.common.base.Preconditions;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Locale;
+
 /**
  * @author krico
  * @since 14/02/15.
@@ -14,6 +16,6 @@ public final class CurrencyUtil {
     public static String formatCurrencyNumber(String currency, Double amount) {
         currency = Preconditions.checkNotNull(StringUtils.trimToNull(currency));
         Preconditions.checkNotNull(amount);
-        return String.format("%.2f", amount);
+        return String.format(Locale.ROOT, "%.2f", amount);
     }
 }
