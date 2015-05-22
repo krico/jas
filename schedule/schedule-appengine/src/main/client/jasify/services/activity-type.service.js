07ff5aa5589c36addf6aae25983034cdd9c2f0be
krico <github@cwa.to>
2015-05-23 00:21:51 +0200

    ActivityType handlers

--- schedule/schedule-appengine/src/main/client/jasify/services/activity-type.service.js
+++ schedule/schedule-appengine/src/main/client/jasify/services/activity-type.service.js
@@ -12,50 +12,40 @@
 
         function query(organizationId) {
             return Endpoint.jasify(function (jasify) {
-                return jasify.activityTypes.query({organizationId: fetchId(organizationId)})
-                    .then(resultHandler, errorHandler);
+                return jasify.activityTypes.query({organizationId: Endpoint.fetchId(organizationId)})
+                    .then(Endpoint.itemsResultHandler, Endpoint.errorHandler);
             });
         }
 
         function get(id) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.get({id: id})
-                    .then(resultHandler, errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
             });
         }
 
         function update(activityType) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.update(activityType)
-                    .then(resultHandler, errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
             });
         }
 
         function add(organizationId, activityType) {
             return Endpoint.jasify(function (jasify) {
-                return jasify.activityTypes.add({organizationId: fetchId(organizationId), activityType: activityType})
-                    .then(resultHandler, errorHandler);
+                return jasify.activityTypes.add({
+                    organizationId: Endpoint.fetchId(organizationId),
+                    activityType: activityType
+                })
+                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
             });
         }
 
         function remove(id) {
             return Endpoint.jasify(function (jasify) {
-                return jasify.activityTypes.remove({id: fetchId(id)})
-                    .then(resultHandler, errorHandler);
+                return jasify.activityTypes.remove({id: Endpoint.fetchId(id)})
+                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
             });
-        }
-
-        function errorHandler(e) {
-            return $q.reject(e);
-        }
-
-        function resultHandler(resp) {
-            return resp.result;
-        }
-
-        function fetchId(o) {
-            if (angular.isObject(o)) return o.id;
-            return o;
         }
 
         return ActivityType;
