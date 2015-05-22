1f3bddc677b67b708b01ffbf17002ffdc4fd78ef
krico <github@cwa.to>
2015-05-23 00:30:52 +0200

    ActivityType handlers

--- schedule/schedule-appengine/src/main/client/jasify/services/activity-type.service.js
+++ schedule/schedule-appengine/src/main/client/jasify/services/activity-type.service.js
@@ -13,21 +13,21 @@
         function query(organizationId) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.query({organizationId: Endpoint.fetchId(organizationId)})
-                    .then(Endpoint.itemsResultHandler, Endpoint.errorHandler);
+                    .then(Endpoint.itemsResultHandler, Endpoint.rejectHandler);
             });
         }
 
         function get(id) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.get({id: id})
-                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.rejectHandler);
             });
         }
 
         function update(activityType) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.update(activityType)
-                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.rejectHandler);
             });
         }
 
@@ -37,14 +37,14 @@
                     organizationId: Endpoint.fetchId(organizationId),
                     activityType: activityType
                 })
-                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.rejectHandler);
             });
         }
 
         function remove(id) {
             return Endpoint.jasify(function (jasify) {
                 return jasify.activityTypes.remove({id: Endpoint.fetchId(id)})
-                    .then(Endpoint.resultHandler, Endpoint.errorHandler);
+                    .then(Endpoint.resultHandler, Endpoint.rejectHandler);
             });
         }
 
