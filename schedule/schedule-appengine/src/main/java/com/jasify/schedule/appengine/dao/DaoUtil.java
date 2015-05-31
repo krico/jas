fa434b26d01b0c94dd35b9584f75a30beac97844
krico <github@cwa.to>
2015-05-31 23:42:36 +0200

    Batch get implemented

--- schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/dao/DaoUtil.java
+++ schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/dao/DaoUtil.java
@@ -3,6 +3,8 @@ package com.jasify.schedule.appengine.dao;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.labs.repackaged.com.google.common.base.Function;
+import com.google.appengine.labs.repackaged.com.google.common.collect.Maps;
 import com.google.common.base.Optional;
 import com.jasify.schedule.appengine.memcache.Memcache;
 import org.slf4j.Logger;
@@ -10,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.slim3.datastore.ModelMeta;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author krico
@@ -35,6 +40,25 @@ public final class DaoUtil {
         return cachePut(id, meta, model, null);
     }
 
+    public static <T> void cachePutAll(@Nonnull Map<Key, T> kvp, @Nonnull final ModelMeta<T> meta) {
+        cachePutAll(kvp, meta, null);
+    }
+
+    public static <T> void cachePutAll(@Nonnull Map<Key, T> kvp, @Nonnull final ModelMeta<T> meta, Expiration expiration) {
+        try {
+            Map<Key, Entity> keyEntityMap = Maps.transformValues(kvp, new Function<T, Entity>() {
+                @Nullable
+                @Override
+                public Entity apply(@Nullable T model) {
+                    return model == null ? null : meta.modelToEntity(model);
+                }
+            });
+            Memcache.putAll(keyEntityMap, expiration);
+        } catch (IllegalArgumentException e) {
+            log.warn("Memcache exception IGNORED", e);
+        }
+    }
+
     public static <T> T cacheGet(@Nonnull Key id, @Nonnull ModelMeta<T> meta) {
         T ret = null;
         try {
@@ -46,6 +70,23 @@ public final class DaoUtil {
             log.warn("Memcache exception IGNORED", e);
         }
         return ret;
+    }
+
+    public static <T> Map<Key, T> cacheGet(@Nonnull List<Key> ids, @Nonnull final ModelMeta<T> meta) {
+        Map<Key, T> modelMap = Maps.newHashMap();
+        try {
+            Map<Key, Entity> cached = Memcache.getAll(ids);
+            modelMap = Maps.transformValues(cached, new Function<Entity, T>() {
+                @Nullable
+                @Override
+                public T apply(Entity entity) {
+                    return meta.entityToModel(entity);
+                }
+            });
+        } catch (IllegalArgumentException e) {
+            log.warn("Memcache exception IGNORED", e);
+        }
+        return modelMap;
     }
 
     /**
--- schedule/schedule-appengine/src/test/java/com/jasify/schedule/appengine/dao/DaoUtilTest.java
+++ schedule/schedule-appengine/src/test/java/com/jasify/schedule/appengine/dao/DaoUtilTest.java
@@ -1,5 +1,6 @@
 package com.jasify.schedule.appengine.dao;
 
+import com.google.api.client.util.Maps;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.memcache.Expiration;
 import com.google.common.base.Optional;
@@ -8,9 +9,11 @@ import io.github.benas.jpopulator.api.Populator;
 import io.github.benas.jpopulator.impl.PopulatorBuilder;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slim3.datastore.Datastore;
+
+import java.util.Arrays;
+import java.util.Map;
 
 import static junit.framework.TestCase.*;
 
@@ -49,7 +52,31 @@ public class DaoUtilTest {
 
     }
 
-//    @Ignore("This test takes too long, but proves the point")
+    @Test
+    public void testMarshallAndUnMarshallALLMemcacheEntries() {
+        Example example1 = createExample();
+        example1.setId(Datastore.allocateId(Example.class));
+        Example example2 = createExample();
+        example2.setId(Datastore.allocateId(Example.class));
+
+        Map<Key, Example> fetchedEmpty = DaoUtil.cacheGet(Arrays.asList(example1.getId(), example2.getId()), META);
+        assertTrue(fetchedEmpty.isEmpty());
+
+        Map<Key, Example> cache = Maps.newHashMap();
+        cache.put(example1.getId(), example1);
+        cache.put(example2.getId(), example2);
+        DaoUtil.cachePutAll(cache, META);
+        assertEquals(example1, DaoUtil.cacheGet(example1.getId(), META));
+        assertEquals(example2, DaoUtil.cacheGet(example2.getId(), META));
+
+        Map<Key, Example> fetched = DaoUtil.cacheGet(Arrays.asList(example1.getId(), example2.getId()), META);
+        assertNotNull(fetched);
+        assertEquals(2, fetched.size());
+        assertEquals(example1, fetched.get(example1.getId()));
+        assertEquals(example2, fetched.get(example2.getId()));
+    }
+
+    //    @Ignore("This test takes too long, but proves the point")
 //    @Test
     public void testPutWithExpiration() throws Exception {
         final int milliDelay = 3000;
