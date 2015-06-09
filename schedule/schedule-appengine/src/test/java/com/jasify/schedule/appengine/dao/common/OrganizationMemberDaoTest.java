0997b069bd5237b79706bd01e5c6c5b5fb0e9f21
krico <github@cwa.to>
2015-06-09 21:44:17 +0200

    Created OrganizationMemberDao as a helper dao for OrganizationDao

---
+++ schedule/schedule-appengine/src/test/java/com/jasify/schedule/appengine/dao/common/OrganizationMemberDaoTest.java
@@ -0,0 +1,60 @@
+package com.jasify.schedule.appengine.dao.common;
+
+import com.google.appengine.api.datastore.Key;
+import com.jasify.schedule.appengine.TestHelper;
+import com.jasify.schedule.appengine.model.common.OrganizationMember;
+import com.jasify.schedule.appengine.model.users.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slim3.datastore.Datastore;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.*;
+
+public class OrganizationMemberDaoTest {
+    private OrganizationMemberDao dao;
+
+    @BeforeClass
+    public static void initialize() {
+        TestHelper.setSystemProperties();
+    }
+
+    @After
+    public void cleanup() {
+        TestHelper.cleanupDatastore();
+    }
+
+    @Before
+    public void resetCache() {
+        TestHelper.initializeDatastore();
+        dao = new OrganizationMemberDao();
+    }
+
+    @Test
+    public void testByUserId() throws Exception {
+        Key key = Datastore.allocateId(User.class);
+
+        OrganizationMember member1 = new OrganizationMember();
+        member1.getUserRef().setKey(key);
+        OrganizationMember member2 = new OrganizationMember();
+        member2.getUserRef().setKey(Datastore.allocateId(User.class));
+        OrganizationMember member3 = new OrganizationMember();
+        member3.getUserRef().setKey(key);
+
+        dao.save(Arrays.asList(member1, member2, member3));
+
+
+        for (int M = 0; M < 3; ++M) {
+            List<OrganizationMember> results = dao.byUserId(key);
+            assertNotNull(results);
+            assertEquals(2, results.size());
+
+            assertTrue(member1.getId().equals(results.get(0).getId()) || member3.getId().equals(results.get(0).getId()));
+            assertTrue(member1.getId().equals(results.get(1).getId()) || member3.getId().equals(results.get(1).getId()));
+        }
+    }
+}
