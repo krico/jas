db77b93494cadf2fd7656286579b35afd36125c0
Waldemar <wszarmach@wp.pl>
2015-08-25 02:35:36 +0200

    Modified Subscribers screen to reflect new UI

--- schedule/schedule-appengine/src/main/client/jasify/admin/activity/admin-subscribers.js
+++ schedule/schedule-appengine/src/main/client/jasify/admin/activity/admin-subscribers.js
@@ -2,20 +2,33 @@
 
     angular.module('jasify.admin').controller('AdminSubscribersController', AdminSubscribersController);
 
-    function AdminSubscribersController($location, $filter, subscriptions, Activity) {
+    function AdminSubscribersController($location, $filter, jasDialogs, subscriptions, Activity) {
         var vm = this;
 
         vm.subscriptions = subscriptions.items;
+        vm.displaySubscriptions = [];
         vm.cancel = cancel;
         vm.back = back;
 
+        vm.getSubscriptions = getSubscriptions;
+
+        vm.pageChanged = pageChanged;
+        vm.perPage = perPage;
+        vm.pagination = {
+            total: 0,
+            page: 1,
+            itemsPerPage: 10,
+            numPages: 1,
+            maxSize: 5
+        };
+
         var $translate = $filter('translate');
+
+        vm.pageChanged();
 
         function cancel(id) {
             Activity.cancelSubscription(id).then(ok, fail);
             function ok(r) {
-                var translation = $translate('SUBSCRIPTION_REMOVED');
-                jasDialogs.success(activityUpdatedTranslation);
                 var newS = [];
                 angular.forEach(vm.subscriptions, function (value, key) {
                     if (id != value.id) {
@@ -23,12 +36,29 @@
                     }
                 }, newS);
                 vm.subscriptions = newS;
+                pageChanged();
+            }
+
+            function fail(r) {
+                var translation = $translate('FAILED_PLEASE_RETRY');
+                jasDialogs.resultError(translation, r);
             }
         }
 
-        function fail(r) {
-            var failedPleaseRetryTranslation = $translate('FAILED_PLEASE_RETRY');
-            jasDialogs.resultError(failedPleaseRetryTranslation, r);
+        function getSubscriptions() {
+            var limit = vm.pagination.itemsPerPage;
+            var offset = (vm.pagination.page - 1) * limit;
+
+            vm.displaySubscriptions = vm.subscriptions.slice(offset, offset + limit);
+            vm.pagination.total = vm.subscriptions.length;
+        }
+
+        function pageChanged() {
+            vm.getSubscriptions();
+        }
+
+        function perPage() {
+            return vm.pagination.itemsPerPage;
         }
 
         function back() {
