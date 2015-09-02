856d74df8c63a204e26d6289203662a9b1213683
Waldemar <wszarmach@wp.pl>
2015-09-02 06:29:44 +0200

    SubscribedActivities

--- schedule/schedule-appengine/src/test/java/com/jasify/schedule/appengine/dao/common/SubscriptionDaoTest.java
+++ schedule/schedule-appengine/src/test/java/com/jasify/schedule/appengine/dao/common/SubscriptionDaoTest.java
@@ -1,14 +1,17 @@
 package com.jasify.schedule.appengine.dao.common;
 
+import com.google.appengine.api.datastore.Key;
 import com.jasify.schedule.appengine.TestHelper;
 import com.jasify.schedule.appengine.model.activity.Activity;
 import com.jasify.schedule.appengine.model.activity.Subscription;
+import com.jasify.schedule.appengine.model.users.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slim3.datastore.Datastore;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.*;
@@ -77,5 +80,67 @@ public class SubscriptionDaoTest {
         assertEquals(2, dao.getByActivity(activity1.getId()).size());
         assertEquals(3, dao.getByActivity(activity2.getId()).size());
         assertEquals(2, dao.getByActivity(activity1.getId()).size());
+    }
+
+    @Test
+    public void testGetByUserWithNullKey() throws Exception {
+        Activity activity = TestHelper.createActivity(true);
+        TestHelper.createSubscription(TestHelper.createUser(true), activity, true);
+        List<Subscription> result = dao.getByUser(null);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetByUserWithUnknownKey() throws Exception {
+        Activity activity = TestHelper.createActivity(true);
+        TestHelper.createSubscription(TestHelper.createUser(true), activity, true);
+        List<Subscription> result = dao.getByUser(Datastore.allocateId(User.class));
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetByUser() throws Exception {
+        User user1 = TestHelper.createUser(true);
+        for (int i = 0; i < 2; i++) {
+            TestHelper.createSubscription(user1, TestHelper.createActivity(true), true);
+        }
+
+        User user2 = TestHelper.createUser(true);
+        for (int i = 0; i < 3; i++) {
+            TestHelper.createSubscription(user2, TestHelper.createActivity(true), true);
+        }
+
+        assertEquals(2, dao.getByUser(user1.getId()).size());
+        assertEquals(3, dao.getByUser(user2.getId()).size());
+        assertEquals(2, dao.getByUser(user1.getId()).size());
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        Activity activity = TestHelper.createActivity(true);
+        User user = TestHelper.createUser(true);
+        Subscription subscription = TestHelper.createSubscription(user, activity, false);
+        subscription.getUserRef().setKey(null);
+
+        Key subscriptionId = dao.save(subscription, user.getId());
+        assertNotNull(subscriptionId);
+        Subscription saveSubscription = dao.get(subscriptionId);
+        assertNotNull(saveSubscription);
+        assertEquals(user.getId(), saveSubscription.getUserRef().getKey());
+    }
+
+    @Test
+    public void testSaveList() throws Exception {
+        Activity activity = TestHelper.createActivity(true);
+        User user = TestHelper.createUser(true);
+
+        List<Subscription> subscriptions = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            subscriptions.add(TestHelper.createSubscription(user, activity, false));
+        }
+
+        List<Key> subscriptionIds = dao.save(subscriptions);
+        assertNotNull(subscriptionIds);
+        assertEquals(subscriptions.size(), subscriptionIds.size());
     }
 }
