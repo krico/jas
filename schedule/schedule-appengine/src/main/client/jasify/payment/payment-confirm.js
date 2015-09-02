3af43ede15499d064feca927ea0c111a65e971e2
krico <github@cwa.to>
2015-09-02 23:29:50 +0200

    Invoice in the web interface is functional, this closes  #674

--- schedule/schedule-appengine/src/main/client/jasify/payment/payment-confirm.js
+++ schedule/schedule-appengine/src/main/client/jasify/payment/payment-confirm.js
@@ -5,9 +5,19 @@
     function PaymentConfirmController($log, $location, $routeParams, BrowserData, Balance, jasDialogs) {
         var vm = this;
         vm.progress = false;
+        vm.success = false;
         vm.invoice = null;
+        vm.next = next;
         vm.getInvoice = getInvoice;
         vm.getInvoice($routeParams.paymentId);
+
+        function next() {
+            $location.replace();
+            $location.search({paymentStatus: 'success'});
+            $location.path(BrowserData.getPaymentAcceptRedirect());
+            BrowserData.clearPaymentAcceptRedirect();
+            BrowserData.clearPaymentCancelRedirect();
+        }
 
         function getInvoice(paymentId) {
             vm.progress = true;
@@ -22,6 +32,7 @@
 
             function fail(reason) {
                 vm.progress = false;
+                vm.success = true;
                 jasDialogs.error('Failed to get invoice from server');
             }
         }
