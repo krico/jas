54fbc6b6b95a3db7d3cc3acfc51f51298f269dd1
Waldemar <wszarmach@wp.pl>
2015-09-24 22:37:06 +0200

    closes #719 Removed restriction on Invoice payments must be 7 days before first activity Extracted PaymentOptions to own class

--- schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/model/cart/ShoppingCart.java
+++ schedule/schedule-appengine/src/main/java/com/jasify/schedule/appengine/model/cart/ShoppingCart.java
@@ -1,12 +1,8 @@
 package com.jasify.schedule.appengine.model.cart;
 
 import com.google.appengine.api.datastore.Key;
-import com.jasify.schedule.appengine.dao.common.ActivityDao;
-import com.jasify.schedule.appengine.model.EntityNotFoundException;
 import com.jasify.schedule.appengine.model.activity.Activity;
 import com.jasify.schedule.appengine.model.activity.ActivityPackage;
-import com.jasify.schedule.appengine.model.payment.InvoicePaymentProvider;
-import com.jasify.schedule.appengine.model.payment.PayPalPaymentProvider;
 import com.jasify.schedule.appengine.model.payment.PaymentTypeEnum;
 import com.jasify.schedule.appengine.util.FormatUtil;
 import org.slf4j.Logger;
@@ -15,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,8 +18,8 @@ import java.util.List;
  * @since 23/03/15.
  */
 public class ShoppingCart implements Serializable {
-    private static final Logger log = LoggerFactory.getLogger(ShoppingCart.class);
-    private final static long InvoiceDaysBeforePaymentRequiredInMillis = 86400000 * 7;
+    private static final Logger Log = LoggerFactory.getLogger(ShoppingCart.class);
+
     private String id;
     private String currency = "CHF";
     private Double total;
@@ -85,64 +80,24 @@ public class ShoppingCart implements Serializable {
         }
         setTotal(total.doubleValue());
 
-        for (PaymentTypeEnum paymentTypeEnum : PaymentTypeEnum.values()) {
-            getPaymentOptions().add(createPaymentOptions(paymentTypeEnum));
+        for (PaymentTypeEnum paymentType : PaymentTypeEnum.values()) {
+            PaymentOption paymentOption = createPaymentOption(paymentType);
+            if (paymentOption != null) {
+                paymentOptions.add(paymentOption);
+            }
         }
         return this;
     }
 
-    private Date getClosestItemDate() {
-        ActivityDao activityDao = new ActivityDao();
-        Date closestDate = null;
-        for (Item item : items) {
-            try {
-                // TODO: Not the best to do this!!! Must fix
-                if (item.getData() != null) {
-                    ArrayList<Key> keyList = (ArrayList<Key>)item.getData();
-                    for (Key key  : keyList) {
-                        Activity activity = activityDao.get(key);
-                        if (closestDate == null || activity.getStart().before(closestDate)) {
-                            closestDate = activity.getStart();
-                        }
-                    }
-                } else {
-                    Activity activity = activityDao.get(item.getItemId());
-                    if (closestDate == null || activity.getStart().before(closestDate)) {
-                        closestDate = activity.getStart();
-                    }
-                }
-            } catch (EntityNotFoundException e) {
-                log.error("Failed to find Activity {} with key {}", item.getDescription(), item.getItemId(), e);
-            }
-        }
-        return closestDate;
-    }
-
-    private PaymentOption createPaymentOptions(PaymentTypeEnum paymentType) {
-        PaymentOption paymentOption = new PaymentOption(paymentType);
-        paymentOption.setEnabled(true);
+    private PaymentOption createPaymentOption(PaymentTypeEnum paymentType) {
         switch (paymentType) {
-            case Invoice:
-                paymentOption.setFee(InvoicePaymentProvider.INVOICE_FEE_FLAT);
-                paymentOption.setFeeReason("<nobr>For Electronic Invoice payments</nobr> there is a handling fee of <b><nobr>CHF 1.00</nobr></b>");
-                Date closestDate = getClosestItemDate();
-                if (closestDate != null && closestDate.getTime() < System.currentTimeMillis() + InvoiceDaysBeforePaymentRequiredInMillis) {
-                    paymentOption.setEnabled(false);
-                    paymentOption.setDisabledReason("For Invoice payments a minimum of 7 days is required before the first activity");
-                }
-                break;
-            case PayPal:
-                paymentOption.setFee(PayPalPaymentProvider.calculateHandlingFee(getTotal()));
-                paymentOption.setFeeReason("<nobr>For PayPal payments there</nobr> is a handling fee of <b>3.5%</b> plus <b><nobr>CHF 0.55</nobr></b>");
-                break;
+            case Cash: return new PaymentOption.CashPaymentOption();
+            case Invoice: return new PaymentOption.InvoicePaymentOption(items);
+            case PayPal: return new PaymentOption.PayPalPaymentOption(getTotal());
             default:
-                paymentOption.setEnabled(false);
-                paymentOption.setFee(BigDecimal.ZERO.doubleValue());
-                // TODO
-                paymentOption.setDisabledReason("Not supported by supplier");
-                break;
+                Log.error("Unknown PaymentType {}", paymentType.name());
+                return null;
         }
-        return paymentOption;
     }
 
     public static class ItemBuilder {
@@ -246,56 +201,4 @@ public class ShoppingCart implements Serializable {
             this.data = data;
         }
     }
-
-    public static class PaymentOption implements Serializable {
-        private PaymentTypeEnum paymentType;
-        private Double fee;
-        private String feeReason;
-        private boolean enabled;
-        private String disabledReason;
-
-        public PaymentOption(PaymentTypeEnum paymentType){
-            setPaymentType(paymentType);
-        }
-        public PaymentTypeEnum getPaymentType() {
-            return paymentType;
-        }
-
-        public void setPaymentType(PaymentTypeEnum paymentType) {
-            this.paymentType = paymentType;
-        }
-
-        public String getFeeReason() {
-            return feeReason;
-        }
-
-        public void setFeeReason(String feeReason) {
-            this.feeReason = feeReason;
-        }
-
-        public Double getFee() {
-            return fee;
-        }
-
-        public void setFee(Double fee) {
-            this.fee = fee;
-        }
-
-        public boolean isEnabled() {
-            return enabled;
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.enabled = enabled;
-        }
-
-        public String getDisabledReason() {
-            return disabledReason;
-        }
-
-        public void setDisabledReason(String disabledReason) {
-            this.disabledReason = disabledReason;
-        }
-    }
-
 }
